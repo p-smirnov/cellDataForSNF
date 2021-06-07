4152cb43c6ed419dcc4626937d7058b9b9b26ba0
Petr Smirnov <psmirnov2000@gmail.com>
2021-06-07 15:44:38 -0400

    added gdsc1 and 2 data distinction

--- downloadCellData.R
+++ downloadCellData.R
@@ -6,36 +6,66 @@ outputDir <- "./output"
 
 if(!file.exists(outputDir)) dir.create(outputDir)
 
-GDSC <- downloadPSet("GDSC_2020(v2-8.2)", saveDir="./dwl/")
+GDSC2 <- downloadPSet("GDSC_2020(v2-8.2)", saveDir="./dwl/")
 
-GDSC.cell.info <- cellInfo(GDSC)[, c("cellid", "tissueid", "Cellosaurus.Disease.Type", "CellLine.Type")]
-GDSC.tissue.disease.map <- unique(GDSC.cell.info[,c("tissueid", 'Cellosaurus.Disease.Type')])
+GDSC2.cell.info <- cellInfo(GDSC2)[, c("cellid", "tissueid", "Cellosaurus.Disease.Type", "CellLine.Type")]
+GDSC2.tissue.disease.map <- unique(GDSC2.cell.info[,c("tissueid", 'Cellosaurus.Disease.Type')])
 
-GDSC.tissue.disease.map <- GDSC.tissue.disease.map[order(GDSC.tissue.disease.map[[1]], GDSC.tissue.disease.map[[2]]),]
-
-
-GDSC.rna.data <- summarizeMolecularProfiles(GDSC, "Kallisto_0.46.1.rnaseq")
-
-GDSC.rna.data <- t(SummarizedExperiment::assay(GDSC.rna.data))
+GDSC2.tissue.disease.map <- GDSC2.tissue.disease.map[order(GDSC2.tissue.disease.map[[1]], GDSC2.tissue.disease.map[[2]]),]
 
 
-GDSC.cnv.data <- summarizeMolecularProfiles(GDSC, "cnv")
+GDSC2.rna.data <- summarizeMolecularProfiles(GDSC2, "Kallisto_0.46.1.rnaseq")
 
-GDSC.cnv.data <- t(SummarizedExperiment::assay(GDSC.cnv.data))
+GDSC2.rna.data <- t(SummarizedExperiment::assay(GDSC2.rna.data))
 
-GDSC.mut.data <- summarizeMolecularProfiles(GDSC, "mutation_exome", summary.stat="or")
 
-GDSC.mut.data <- t(SummarizedExperiment::assay(GDSC.mut.data))
+GDSC2.cnv.data <- summarizeMolecularProfiles(GDSC2, "cnv")
 
-GDSC.aac.data <- t(summarizeSensitivityProfiles(GDSC, "aac_recomputed"))
+GDSC2.cnv.data <- t(SummarizedExperiment::assay(GDSC2.cnv.data))
 
-write.csv(GDSC.cell.info, file=file.path(outputDir, "GDSC_cell_info.csv"))
-write.csv(GDSC.tissue.disease.map, file=file.path(outputDir, "GDSC_tissue_table.csv"))
-write.csv(GDSC.rna.data, file=file.path(outputDir, "GDSC_rna_data.csv"))
-write.csv(GDSC.cnv.data, file=file.path(outputDir, "GDSC_cnv_data.csv"))
-write.csv(GDSC.mut.data, file=file.path(outputDir, "GDSC_mut_data.csv"))
-write.csv(GDSC.aac.data, file=file.path(outputDir, "GDSC_aac_data.csv"))
+GDSC2.mut.data <- summarizeMolecularProfiles(GDSC2, "mutation_exome", summary.stat="or")
 
+GDSC2.mut.data <- t(SummarizedExperiment::assay(GDSC2.mut.data))
+
+GDSC2.aac.data <- t(summarizeSensitivityProfiles(GDSC2, "aac_recomputed"))
+
+write.csv(GDSC2.cell.info, file=file.path(outputDir, "GDSC2_cell_info.csv"))
+write.csv(GDSC2.tissue.disease.map, file=file.path(outputDir, "GDSC2_tissue_table.csv"))
+write.csv(GDSC2.rna.data, file=file.path(outputDir, "GDSC2_rna_data.csv"))
+write.csv(GDSC2.cnv.data, file=file.path(outputDir, "GDSC2_cnv_data.csv"))
+write.csv(GDSC2.mut.data, file=file.path(outputDir, "GDSC2_mut_data.csv"))
+write.csv(GDSC2.aac.data, file=file.path(outputDir, "GDSC2_aac_data.csv"))
+
+
+GDSC1 <- downloadPSet("GDSC_2020(v1-8.2)", saveDir="./dwl/")
+
+GDSC1.cell.info <- cellInfo(GDSC1)[, c("cellid", "tissueid", "Cellosaurus.Disease.Type", "CellLine.Type")]
+GDSC1.tissue.disease.map <- unique(GDSC1.cell.info[,c("tissueid", 'Cellosaurus.Disease.Type')])
+
+GDSC1.tissue.disease.map <- GDSC1.tissue.disease.map[order(GDSC1.tissue.disease.map[[1]], GDSC1.tissue.disease.map[[2]]),]
+
+
+GDSC1.rna.data <- summarizeMolecularProfiles(GDSC1, "Kallisto_0.46.1.rnaseq")
+
+GDSC1.rna.data <- t(SummarizedExperiment::assay(GDSC1.rna.data))
+
+
+GDSC1.cnv.data <- summarizeMolecularProfiles(GDSC1, "cnv")
+
+GDSC1.cnv.data <- t(SummarizedExperiment::assay(GDSC1.cnv.data))
+
+GDSC1.mut.data <- summarizeMolecularProfiles(GDSC1, "mutation_exome", summary.stat="or")
+
+GDSC1.mut.data <- t(SummarizedExperiment::assay(GDSC1.mut.data))
+
+GDSC1.aac.data <- t(summarizeSensitivityProfiles(GDSC1, "aac_recomputed"))
+
+write.csv(GDSC1.cell.info, file=file.path(outputDir, "GDSC1_cell_info.csv"))
+write.csv(GDSC1.tissue.disease.map, file=file.path(outputDir, "GDSC1_tissue_table.csv"))
+write.csv(GDSC1.rna.data, file=file.path(outputDir, "GDSC1_rna_data.csv"))
+write.csv(GDSC1.cnv.data, file=file.path(outputDir, "GDSC1_cnv_data.csv"))
+write.csv(GDSC1.mut.data, file=file.path(outputDir, "GDSC1_mut_data.csv"))
+write.csv(GDSC1.aac.data, file=file.path(outputDir, "GDSC1_aac_data.csv"))
 
 
 CCLE <- downloadPSet("CCLE_2015", saveDir="./dwl/")
